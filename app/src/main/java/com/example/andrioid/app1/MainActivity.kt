81927153d602a133a3c6148db12b04567e04399c
Adrian <rudzki_96@o2.pl>
2019-03-13 03:30:21 +0100

    .

---
+++ app/src/main/java/com/example/andrioid/app1/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.andrioid.app1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
